26a487c03143d3e8b9b5182f034a17aecfe3fa69
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-03-09 14:56:40 -0300

    setup config kotlin and spring

---
+++ src/main/kotlin/com/pethat/backendk/BackendkApplication.kt
@@ -0,0 +1,11 @@
+package com.pethat.backendk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendkApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendkApplication>(*args)
+}
---
+++ src/test/kotlin/com/pethat/backendk/BackendkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pethat.backendk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
