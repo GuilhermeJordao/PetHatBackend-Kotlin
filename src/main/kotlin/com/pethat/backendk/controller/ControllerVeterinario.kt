fce27c0f550c08e6736ee38e1270c36d0cadf239
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-03-18 23:48:08 -0300

    Created for controller the endpoints of search and insertion veterinario

--- src/main/kotlin/com/pethat/backendk/controller/ControllerVeterinario.kt
+++ src/main/kotlin/com/pethat/backendk/controller/ControllerVeterinario.kt
@@ -1,4 +1,29 @@
 package com.pethat.backendk.controller
 
-class ControllerVeterinario {
+import com.pethat.backendk.model.Veterinario
+import com.pethat.backendk.service.VeterinarioService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/veterinario")
+class ControllerVeterinario(val service: VeterinarioService) {
+
+    @GetMapping("/{email}")
+    fun findByEmail(@PathVariable email:String): ResponseEntity<Veterinario> {
+        val veterinario = service.findByEmail(email)
+        return ResponseEntity(veterinario, HttpStatus.OK)
+    }
+
+    @PostMapping("/")
+    fun insertVeterinario(@RequestBody veterinario: Veterinario): ResponseEntity<Veterinario>{
+        val veterinario = service.insertVeterinario(veterinario)
+        return ResponseEntity(veterinario, HttpStatus.CREATED)
+    }
 }
