d9880a879956f560ecd9419c307f6ea83685d8d0
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-03-18 23:45:42 -0300

    Created service for repository of veterinario and for controller

--- src/main/kotlin/com/pethat/backendk/service/VeterinarioService.kt
+++ src/main/kotlin/com/pethat/backendk/service/VeterinarioService.kt
@@ -1,2 +1,12 @@
-package com.pethat.backendk.service 
+package com.pethat.backendk.service
 
+import com.pethat.backendk.model.Veterinario
+import com.pethat.backendk.repository.VeterinarioRepository
+import org.springframework.stereotype.Service
+
+@Service
+class VeterinarioService(val repository: VeterinarioRepository) {
+    fun findByEmail(email: String): Veterinario? = repository.findByEmail(email)
+    fun insertVeterinario(veterinario: Veterinario): Veterinario = repository.insert(veterinario)
+
+}
