1ba12e6b2224d658669b45db736cf3d1800c333d
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-03-20 14:24:37 -0300

    fix: adjusted attribute of data class consulta

--- src/main/kotlin/com/pethat/backendk/model/Consulta.kt
+++ src/main/kotlin/com/pethat/backendk/model/Consulta.kt
@@ -1,6 +1,12 @@
 package com.pethat.backendk.model
 
 import com.pethat.backendk.model.enums.StatusConsulta
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
-
-data class Consulta(val data: LocalDateTime, val hora: LocalDateTime, val status: StatusConsulta)
+@Document("consulta")
+data class Consulta(
+    val data: LocalDateTime,
+    val hora: LocalDateTime,
+    val status: StatusConsulta,
+    val veterinario: Veterinario
+)
