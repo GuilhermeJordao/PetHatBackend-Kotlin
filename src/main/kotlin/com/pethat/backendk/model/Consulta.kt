bb764820ec511c664dc2a7d46d4d102797de2bbe
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-03-20 14:19:10 -0300

    Revert "fix: adjusted attribute of data class veterinario"
    This reverts commit 9f22a8efc0dc51f6720569ff1445771e2a2aaa0f.

--- src/main/kotlin/com/pethat/backendk/model/Consulta.kt
+++ src/main/kotlin/com/pethat/backendk/model/Consulta.kt
@@ -1,12 +1,6 @@
 package com.pethat.backendk.model
 
 import com.pethat.backendk.model.enums.StatusConsulta
-import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
-@Document("consulta")
-data class Consulta(
-    val data: LocalDateTime,
-    val hora: LocalDateTime,
-    val status: StatusConsulta,
-    val veterinario: Veterinario
-)
+
+data class Consulta(val data: LocalDateTime, val hora: LocalDateTime, val status: StatusConsulta)
